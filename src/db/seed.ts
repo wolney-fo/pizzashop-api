73a7eeaed224bd8360e4d043c829f03ebf731bcf
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-22 12:46:41 -0300

    chore: add seeding script to populate development database

---
+++ src/db/seed.ts
@@ -0,0 +1,69 @@
+import { faker } from '@faker-js/faker'
+import chalk from 'chalk'
+import { db } from './connection'
+import { restaurants, users } from './schema'
+
+/**
+ * Reset database
+ */
+
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.blueBright('✅ Database reset.'))
+
+/**
+ * Create customers
+ */
+
+await db.insert(users).values([
+	{
+		name: faker.person.fullName(),
+		email: faker.internet.email(),
+		role: 'customer',
+	},
+	{
+		name: faker.person.fullName(),
+		email: faker.internet.email(),
+		role: 'customer',
+	},
+])
+
+console.log(chalk.blueBright('✅ Created customers.'))
+
+/**
+ * Create manager
+ */
+
+const [manager] = await db
+	.insert(users)
+	.values([
+		{
+			name: faker.person.fullName(),
+			email: 'admin@admin.com',
+			role: 'manager',
+		},
+	])
+	.returning({
+		id: users.id,
+	})
+
+console.log(chalk.blueBright('✅ Created manager.'))
+
+/**
+ * Create restaurant
+ */
+
+await db.insert(restaurants).values([
+	{
+		name: faker.company.name(),
+		description: faker.lorem.paragraph(),
+		managerId: manager.id,
+	},
+])
+
+console.log(chalk.blueBright('✅ Created restaurant.'))
+
+console.log(chalk.greenBright('✅ Database seeded successfully.'))
+
+process.exit()
