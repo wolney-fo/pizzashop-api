cfff706b914ecde3c169cf0b328be39e493d3cac
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-22 13:37:32 -0300

    feat: create create restaurant route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,43 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 import { env } from '../env'
 
 const app = new Elysia()
 
-app.get('/', () => {
-	return 'Hello World'
-})
+app.post(
+	'/restaurants',
+	async ({ body, set }) => {
+		const { restaurantName, managerName, email, phone } = body
+
+		const [manager] = await db
+			.insert(users)
+			.values({
+				name: managerName,
+				email,
+				phone,
+				role: 'manager',
+			})
+			.returning({
+				id: users.id,
+			})
+
+		await db.insert(restaurants).values({
+			name: restaurantName,
+			managerId: manager.id,
+		})
+
+		set.status = 204
+	},
+	{
+		body: t.Object({
+			restaurantName: t.String(),
+			managerName: t.String(),
+			email: t.String({ format: 'email' }),
+			phone: t.String(),
+		}),
+	}
+)
 
 app.listen(env.PORT, () => {
 	console.log('HTTP Server running.')
