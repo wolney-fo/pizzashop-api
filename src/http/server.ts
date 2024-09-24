5061205d9ab8dd44b6327603243af6d888af5202
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-23 22:45:34 -0300

    refactor: separate routes into http directory

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,43 +1,8 @@
-import { Elysia, t } from 'elysia'
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
+import { Elysia } from 'elysia'
 import { env } from '../env'
+import { registerRestaurant } from './routes/restaurants/register-restaurant'
 
-const app = new Elysia()
-
-app.post(
-	'/restaurants',
-	async ({ body, set }) => {
-		const { restaurantName, managerName, email, phone } = body
-
-		const [manager] = await db
-			.insert(users)
-			.values({
-				name: managerName,
-				email,
-				phone,
-				role: 'manager',
-			})
-			.returning({
-				id: users.id,
-			})
-
-		await db.insert(restaurants).values({
-			name: restaurantName,
-			managerId: manager.id,
-		})
-
-		set.status = 204
-	},
-	{
-		body: t.Object({
-			restaurantName: t.String(),
-			managerName: t.String(),
-			email: t.String({ format: 'email' }),
-			phone: t.String(),
-		}),
-	}
-)
+const app = new Elysia().use(registerRestaurant)
 
 app.listen(env.PORT, () => {
 	console.log('HTTP Server running.')
