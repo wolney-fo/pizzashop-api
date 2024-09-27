877c073d0b3cd059db2526a419f4dcff7ec6ec5a
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-26 22:17:25 -0300

    feat: create log out route

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,13 +1,33 @@
+import { Elysia, t, type Static } from 'elysia'
 import jwt from '@elysiajs/jwt'
-import Elysia, { t } from 'elysia'
+
 import { env } from '../env'
 
-export const auth = new Elysia().use(
-	jwt({
-		secret: env.JWT_SECRET_KEY,
-		schema: t.Object({
-			sub: t.String(),
-			restaurantId: t.Optional(t.String()),
-		}),
+const jwtPayload = t.Object({
+	sub: t.String(),
+	restauranteId: t.Optional(t.String()),
+})
+
+export const auth = new Elysia()
+	.use(
+		jwt({
+			secret: env.JWT_SECRET_KEY,
+			schema: jwtPayload,
+		})
+	)
+	.derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
+		return {
+			signUser: async (payload: Static<typeof jwtPayload>) => {
+				const token = await jwt.sign(payload)
+
+				auth.value = token
+				auth.httpOnly = true
+				auth.maxAge = 60 * 60 * 24 * 7 // 7 days
+				auth.path = '/'
+			},
+
+			signOut: async () => {
+				auth.remove()
+			},
+		}
 	})
-)
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,12 +2,14 @@ import { Elysia } from 'elysia'
 import { env } from '../env'
 import { authenticateFromLink } from './routes/auth-links/authenticate-from-link'
 import { sendAuthLink } from './routes/auth-links/send-auth-link'
+import { signOut } from './routes/auth-links/sign-out'
 import { registerRestaurant } from './routes/restaurants/register-restaurant'
 
 const app = new Elysia()
 	.use(registerRestaurant)
 	.use(sendAuthLink)
 	.use(authenticateFromLink)
+	.use(signOut)
 
 app.listen(env.PORT, () => {
 	console.log('HTTP Server running.')
