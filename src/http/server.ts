722454c66d44327f877945099d8b7e731fb673d5
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-26 22:51:34 -0300

    feat: create get current user managed restaurant route

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import { authenticateFromLink } from './routes/auth-links/authenticate-from-link
 import { sendAuthLink } from './routes/auth-links/send-auth-link'
 import { signOut } from './routes/auth-links/sign-out'
 import { registerRestaurant } from './routes/restaurants/register-restaurant'
+import { getManagedRestaurant } from './routes/users/get-managed-restaurant'
 import { getProfile } from './routes/users/get-profile'
 
 const app = new Elysia()
@@ -12,6 +13,7 @@ const app = new Elysia()
 	.use(authenticateFromLink)
 	.use(signOut)
 	.use(getProfile)
+	.use(getManagedRestaurant)
 
 app.listen(env.PORT, () => {
 	console.log('HTTP Server running.')
