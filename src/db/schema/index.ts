54accc283a0c5175c9177f200758186b80eea847
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-27 23:57:40 -0300

    feat: create products, orders and order items tables schemas

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,3 +1,7 @@
 export * from './auth-links'
-export * from './users'
+export * from './order-items'
+export * from './orders'
+export * from './products'
 export * from './restaurants'
+export * from './users'
+
