19963edfd9b1c9396eee4a9a980adb96b5a1fb3d
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-09-28 23:45:25 -0300

    feat: create tables relations

--- src/db/schema/products.ts
+++ src/db/schema/products.ts
@@ -1,6 +1,8 @@
 import { createId } from '@paralleldrive/cuid2'
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { restaurants } from './restaurants'
+import { relations } from 'drizzle-orm'
+import { orderItems } from './order-items'
 
 export const products = pgTable('products', {
 	id: text('id')
@@ -16,4 +18,15 @@ export const products = pgTable('products', {
 		}),
 	createdAt: timestamp('created_at').notNull().defaultNow(),
 	updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+export const productsRelations = relations(products, ({ one, many }) => {
+	return {
+		restaurant: one(restaurants, {
+			fields: [products.restaurantId],
+			references: [restaurants.id],
+			relationName: 'product_restaurant',
+		}),
+		orderItems: many(orderItems),
+	}
 })
--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -2,6 +2,8 @@ import { text, timestamp, pgTable } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
 import { users } from './users'
 import { relations } from 'drizzle-orm'
+import { orders } from './orders'
+import { products } from './products'
 
 export const restaurants = pgTable('restaurants', {
 	id: text('id')
@@ -16,12 +18,14 @@ export const restaurants = pgTable('restaurants', {
 	updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
 
-export const restaurantsRelations = relations(restaurants, ({ one }) => {
+export const restaurantsRelations = relations(restaurants, ({ one, many }) => {
 	return {
 		manager: one(users, {
 			fields: [restaurants.managerId],
 			references: [users.id],
 			relationName: 'restaurant_manager',
 		}),
+		orders: many(orders),
+		products: many(products),
 	}
 })
