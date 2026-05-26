b83fc6e04d5abbd041750a396a5bb31882e38861
Wolney Oliveira <wolney.js@outlook.com>
2026-05-25 22:41:49 -0300

    feat: setup drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import { env } from "./src/env";
+
+export default defineConfig({
+  out: "./src/db/migrations",
+  schema: "./src/db/schema/index.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
