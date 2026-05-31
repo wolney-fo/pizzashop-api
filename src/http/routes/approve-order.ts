7d7c172ca91442a954aac9403fa22e8163ac1f6c
Wolney Oliveira <wolney.js@outlook.com>
2026-05-30 21:21:24 -0300

    feat: implement approve order route

---
+++ src/http/routes/approve-order.ts
@@ -0,0 +1,48 @@
+import Elysia from "elysia";
+import { auth } from "../auth";
+import { z } from "zod";
+import { UnauthorizedError } from "../errors/unauthorized-error";
+import { db } from "../../db";
+import { orders } from "../../db/schema";
+import { eq } from "drizzle-orm";
+
+export const approveOrder = new Elysia().use(auth).patch(
+  "/orders/:orderId/approve",
+  async ({ getCurrentUser, params, set }) => {
+    const { orderId } = params;
+
+    const { restaurantId } = await getCurrentUser();
+
+    if (!restaurantId) {
+      throw new UnauthorizedError();
+    }
+
+    const order = await db.query.orders.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.id, orderId);
+      },
+    });
+
+    if (!order) {
+      set.status = 400;
+
+      return { message: "Order not found" };
+    }
+
+    if (order.status !== "pending") {
+      set.status = 400;
+
+      return { message: "Order is not pending" };
+    }
+
+    await db
+      .update(orders)
+      .set({
+        status: "processing",
+      })
+      .where(eq(orders.id, orderId));
+  },
+  {
+    params: z.object({ orderId: z.string() }),
+  },
+);
--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,7 @@ import { signOut } from "./routes/sign-out";
 import { getUserProfile } from "./routes/get-user-profile";
 import { getManagedRestaurant } from "./routes/get-managed-restaurant";
 import { getOrderDetails } from "./routes/get-order-details";
+import { approveOrder } from "./routes/approve-order";
 
 const app = new Elysia()
   .use(createRestaurant)
@@ -14,7 +15,8 @@ const app = new Elysia()
   .use(signOut)
   .use(getUserProfile)
   .use(getManagedRestaurant)
-  .use(getOrderDetails);
+  .use(getOrderDetails)
+  .use(approveOrder);
 
 app.listen(3333, () => {
   console.log("HTTP Server running");
